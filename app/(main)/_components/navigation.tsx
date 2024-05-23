897b209f0ab565f3f171cf59f0a940c757e398c3
proxza <proxza@gmail.com>
2024-05-23 19:20:44 +1000

    Updates

---
+++ app/(main)/_components/navigation.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+export const Navigation = () => {
+  return (
+    <>
+      <aside className="group/sidebar h-full bg-secondary">
+        <div>
+          <p>Action items</p>
+        </div>
+        <div className="mt-4">
+          <p>Documents</p>
+        </div>
+        <div className="opacity-0 group-hover/sidebar:opacity-100 transition cursor-ew-resize absolute h-full w-1 bg-primary/10 right-0 top-0" />
+      </aside>
+    </>
+  );
+};
