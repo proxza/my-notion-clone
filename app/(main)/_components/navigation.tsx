adae832c5e317bb36d40f12bbd429898d63384fc
proxza <proxza@gmail.com>
2024-05-25 19:58:28 +1000

    Updates

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { cn } from "@/lib/utils";
-import { ChevronsLeft } from "lucide-react";
+import { ChevronsLeft, MenuIcon } from "lucide-react";
 import { usePathname } from "next/navigation";
 import { ElementRef, useRef, useState } from "react";
 import { useMediaQuery } from "usehooks-ts";
@@ -30,6 +30,9 @@ export const Navigation = () => {
         </div>
         <div className="opacity-0 group-hover/sidebar:opacity-100 transition cursor-ew-resize absolute h-full w-1 bg-primary/10 right-0 top-0" />
       </aside>
+      <div ref={navbarRef} className={cn("absolute top-0 z-[99999] left-60 w-[calc(100%-240px)]", isResetting && "transition-all ease-in-out duration-300", isMobile && "left-0 w-full")}>
+        <nav>{isCollapsed && <MenuIcon role="button" className="h-6 w-6 text-muted-foreground" />}</nav>
+      </div>
     </>
   );
 };
