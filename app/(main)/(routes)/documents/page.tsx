e2c2503007f6a975a2e28cb45c0302dfdc57f546
proxza <proxza@gmail.com>
2024-05-27 21:57:13 +1000

    Upd

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,5 +1,16 @@
+import Image from "next/image";
+import { useUser } from "@clerk/clerk-react";
+
 const DocumentsPage = () => {
-  return <div>This is a protected page!</div>;
+  const { user } = useUser();
+
+  return (
+    <div className="h-full flex flex-col items-center justify-center space-y-4">
+      <Image src="/empty.png" height="300" width="300" alt="Empty" className="dark:hidden" />
+      <Image src="/empty-dark.png" height="300" width="300" alt="Empty" className="hidden dark:block" />
+      <h2>Welcome to {user?.firstName}&apos;s Hub</h2>
+    </div>
+  );
 };
 
 export default DocumentsPage;
