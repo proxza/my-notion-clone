481e2464f6ce8bc0fc94b2112fed6921b21e01e1
proxza <proxza@gmail.com>
2024-05-28 10:17:09 +1000

    small updates

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import Image from "next/image";
 import { useUser } from "@clerk/clerk-react";
 
@@ -8,7 +9,7 @@ const DocumentsPage = () => {
     <div className="h-full flex flex-col items-center justify-center space-y-4">
       <Image src="/empty.png" height="300" width="300" alt="Empty" className="dark:hidden" />
       <Image src="/empty-dark.png" height="300" width="300" alt="Empty" className="hidden dark:block" />
-      <h2>Welcome to {user?.firstName}&apos;s Hub</h2>
+      <h2 className="text-lg font-medium">Welcome to {user?.firstName}&apos;s Hub</h2>
     </div>
   );
 };
