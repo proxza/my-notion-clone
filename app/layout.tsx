7c370f6952f930c1c41eff3e4462a63d6e8cd21f
proxza <proxza@gmail.com>
2024-05-28 12:17:55 +1000

    Add some schema for convex db

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,11 +3,12 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/providers/theme-provider";
 import { ConvexClientProvider } from "@/components/providers/convex-provider";
+import { Toaster } from "sonner";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "VisionHub",
+  title: "VisionHUB",
   description: "Hub for your Ideas, Documents and Plans!",
   icons: {
     icon: [
@@ -35,6 +36,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <ConvexClientProvider>
           <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange storageKey="visionhub-theme">
+            <Toaster position="bottom-center" />
             {children}
           </ThemeProvider>
         </ConvexClientProvider>
