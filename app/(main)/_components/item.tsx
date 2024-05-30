a1d62ff68bad6e0d2d49e5c809143876e5a4ee83
proxza <proxza@gmail.com>
2024-05-30 18:16:21 +1000

    Add archive functions

--- app/(main)/_components/item.tsx
+++ app/(main)/_components/item.tsx
@@ -1,9 +1,13 @@
 "use client";
 
 import { Skeleton } from "@/components/ui/skeleton";
+import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
 import { cn } from "@/lib/utils";
-import { ChevronDown, ChevronRight, LucideIcon } from "lucide-react";
+import { useMutation } from "convex/react";
+import { ChevronDown, ChevronRight, LucideIcon, Plus } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
 
 interface ItemProps {
   id?: Id<"documents">;
@@ -19,11 +23,36 @@ interface ItemProps {
 }
 
 export const Item = ({ id, label, onClick, icon: Icon, active, documentIcon, isSearch, level = 0, onExpand, expanded }: ItemProps) => {
+  const router = useRouter();
+  const create = useMutation(api.documents.create);
+
+  const handleExpand = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    event.stopPropagation();
+    onExpand?.();
+  };
+
+  const onCreate = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    event.stopPropagation();
+    if (!id) return;
+    const promise = create({ title: "Untitled", parentDocument: id }).then((documentId) => {
+      if (!expanded) {
+        onExpand?.();
+      }
+      //router.push(`/documents/${documentId}`);
+    });
+
+    toast.promise(promise, {
+      loading: "Creating a new note...",
+      success: "New note created!",
+      error: "Failed to create a new note.",
+    });
+  };
+
   const ChevronIcon = expanded ? ChevronDown : ChevronRight;
   return (
     <div onClick={onClick} role="button" style={{ paddingLeft: level ? `${level * 12 + 12}px` : "12px" }} className={cn("group min-h-[27px] text-sm text-muted-foreground py-1 pr-3 w-full hover:bg-primary/5 flex items-center font-medium", active && "bg-primary/5 text-primary")}>
       {!!id && (
-        <div role="button" className="h-full rounded-sm hover:bg-neutral-300 dark:bg-neutral-600 mr-1" onClick={() => {}}>
+        <div role="button" className="h-full rounded-sm hover:bg-neutral-300 dark:bg-neutral-600 mr-1" onClick={handleExpand}>
           <ChevronIcon className="h-4 w-4 shrink-0 text-muted-foreground/50" />
         </div>
       )}
@@ -33,6 +62,13 @@ export const Item = ({ id, label, onClick, icon: Icon, active, documentIcon, isS
         <kbd className="ml-auto pointer-events-none inline-flex h-5 select-none items-center gap 1 rounded border bg-mutd px-1.5 font-mono text-[10px] font-medium text-muted-foreground opacity-100">
           <span className="text-sx">COMMAND</span> + K
         </kbd>
+      )}
+      {!!id && (
+        <div className="ml-auto flex items-center gap-x-2">
+          <div role="button" onClick={onCreate} className="opacity-0 group-hover:opacity-100 h-full ml-auto rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600">
+            <Plus className="h-4 w-4 text-muted-foreground" />
+          </div>
+        </div>
       )}
     </div>
   );
