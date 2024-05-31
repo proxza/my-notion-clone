fa4f00ab5634e84832f716d73fdc3fc9c1e0bc9c
proxza <proxza@gmail.com>
2024-06-01 09:24:32 +1000

    updates

--- app/(main)/_components/trash-box.tsx
+++ app/(main)/_components/trash-box.tsx
@@ -1,9 +1,11 @@
 "use client";
 
 import { Spinner } from "@/components/spinner";
+import { Input } from "@/components/ui/input";
 import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
 import { useMutation, useQuery } from "convex/react";
+import { Search } from "lucide-react";
 import { useParams, useRouter } from "next/navigation";
 import { useState } from "react";
 import { toast } from "sonner";
@@ -20,6 +22,10 @@ export const TrashBox = () => {
   const filteredDocuments = documents?.filter((document) => {
     return document.title.toLowerCase().includes(search.toLowerCase());
   });
+
+  const onClick = (documentId: string) => {
+    router.push(`/documents/${documentId}`);
+  };
 
   const onRestore = (event: React.MouseEvent<HTMLDivElement, MouseEvent>, documentId: Id<"documents">) => {
     event.stopPropagation();
@@ -54,5 +60,20 @@ export const TrashBox = () => {
     );
   }
 
-  return <div>kek</div>;
+  return (
+    <div className="text-sm">
+      <div className="flex items-center gap-x-1 p-2">
+        <Search className="h-4 w-4" />
+        <Input value={search} onChange={(e) => setSearch(e.target.value)} className="h-7 px-2 focus-visible:ring-transparent bg-secondary" placeholder="Filter by page title..." />
+      </div>
+      <div className="mt-2 px-1 pb-1">
+        <p className="hidden last:block text-xs text-center text-muted-foreground pb-2">No documents found.</p>
+        {filteredDocuments?.map((document) => (
+          <div key={document._id} role="button" onClick={() => onClick(document._id)} className="text-sm rounded-sm w-full hover:bg-primary/5 flex items-center text-primary justify-between">
+            <span>{document.title}</span>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
