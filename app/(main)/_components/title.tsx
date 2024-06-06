c7f2224cafe3c2d61d52710820d0acdb202f8587
proxza <proxza@gmail.com>
2024-06-06 18:26:26 +1000

    upd

--- app/(main)/_components/title.tsx
+++ app/(main)/_components/title.tsx
@@ -5,15 +5,26 @@ import { Input } from "@/components/ui/input";
 import { api } from "@/convex/_generated/api";
 import { Doc } from "@/convex/_generated/dataModel";
 import { useMutation } from "convex/react";
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 interface TitleProps {
   initialData: Doc<"documents">;
 }
 
 export const Title = ({ initialData }: TitleProps) => {
+  const inputRef = useRef<HTMLInputElement>(null);
   const update = useMutation(api.documents.update);
-  const [isEditing, setIsEdition] = useState(false);
+  const [isEditing, setIsEditing] = useState(false);
+  const [title, setTitle] = useState(initialData.title || "Untitled");
+
+  const enableInput = () => {
+    setTitle(initialData.title);
+    setIsEditing(true);
+    setTimeout(() => {
+      inputRef.current?.focus();
+      inputRef.current?.setSelectionRange(0, inputRef.current.value.length);
+    }, 0);
+  };
 
   return (
     <div className="flex items-center gap-x-1">
